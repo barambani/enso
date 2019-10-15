d4ce8026e4c36e0aa22fd5322effd9ba72e1401a
mwu <mwu-tow@gazeta.pl>
2019-10-15 14:45:28 +0200

    innocent code change

--- Pkg/src/main/scala/org/enso/pkg/Main.scala
+++ Pkg/src/main/scala/org/enso/pkg/Main.scala
@@ -3,7 +3,7 @@ package org.enso.pkg
 import java.io.File
 
 object Main extends App {
-  Package.getOrCreate(new File("/Users/marcinkostrzewa/765Luna__$%%$#Project"))
-  Package.getOrCreate(new File("/Users/marcinkostrzewa/proper_%%$##%#project"))
-  Package.getOrCreate(new File("/Users/marcinkostrzewa/Properproject"))
+  Package.getOrCreate(new File("/Users/mwu/765Luna__$%%$#Project"))
+  Package.getOrCreate(new File("/Users/mwu/proper_%%$##%#project"))
+  Package.getOrCreate(new File("/Users/mwu/Properproject"))
 }
